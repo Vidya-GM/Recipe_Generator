c691921fa00fe10df177950ef61ec1a848db74c5
Vidya-GM <130486479+Vidya-GM@users.noreply.github.com>
2025-05-16 14:02:24 +0200

    spell correction

--- culinary_cloud_ai/applications/recipes/models.py
+++ culinary_cloud_ai/applications/recipes/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 # Create your models here.
 class Cuisine(models.Model):
-    cusine_name = models.CharField(max_length=100, unique=True)
+    cuisine_name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.cusine_name
