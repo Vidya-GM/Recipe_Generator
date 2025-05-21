8464d6e31a206af536a9b62787f7e41272415dc1
Vidya <vidyag10@gmail.com>
2025-05-21 12:22:26 +0200

    Add Ingredients Model

--- culinary_cloud_ai/applications/recipes/admin.py
+++ culinary_cloud_ai/applications/recipes/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from applications.recipes.models import Recipe, CookingTime, Cuisine
+from applications.recipes.models import Recipe, CookingTime, Cuisine, Ingredients
 
 # Register your models here.
 
 admin.site.register(Recipe)
 admin.site.register(CookingTime)
 admin.site.register(Cuisine)
+admin.site.register(Ingredients)
--- culinary_cloud_ai/applications/recipes/models.py
+++ culinary_cloud_ai/applications/recipes/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
 
 # Create your models here.
 class Cuisine(models.Model):
@@ -15,6 +13,12 @@ class CookingTime(models.Model):
 
     def __str__(self):
         return f"{self.time_in_minutes} minutes"
+    
+class Ingredients(models.Model):
+    ingredient_name = models.CharField(max_length=150, unique=True)
+
+    def __str__(self):
+        return self.ingredient_name
 
 
 class Recipe(models.Model):
@@ -26,12 +30,12 @@ class Recipe(models.Model):
     recipe_owner = models.ForeignKey("recipe_user.RecipeUser", null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    ingredients = models.TextField()
+    ingredients = models.ManyToManyField(Ingredients, blank=False, related_name="recipes")
     instructions = models.TextField()
     recipe_image = models.ImageField(upload_to='recipes_pic/', null=True, blank=True)
     difficulty = models.CharField(max_length=10, choices=DIFFICULTY_CHOICES)
-    cuisine = models.ForeignKey(Cuisine, on_delete=models.SET_NULL, null=True, blank=True)
-    cooking_time = models.ForeignKey(CookingTime, on_delete=models.SET_NULL, null=True, blank=True)
+    cuisine = models.ForeignKey(Cuisine, on_delete=models.SET_NULL, null=True, blank=True, related_name="recipes")
+    cooking_time = models.ForeignKey(CookingTime, on_delete=models.SET_NULL, null=True, blank=True, related_name="recipes")
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
