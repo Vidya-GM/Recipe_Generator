ff987645d83e22c2e23fb456f55a4280e0e52433
Vidya <vidyag10@gmail.com>
2025-05-22 09:46:06 +0200

    register new model, and migrate

--- culinary_cloud_ai/applications/recipes/admin.py
+++ culinary_cloud_ai/applications/recipes/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from applications.recipes.models import Recipe, CookingTime, Cuisine, Ingredients
+from applications.recipes.models import Recipe, CookingTime, Cuisine, CheckboxIngredient
 
 # Register your models here.
 
 admin.site.register(Recipe)
 admin.site.register(CookingTime)
 admin.site.register(Cuisine)
-admin.site.register(Ingredients)
+admin.site.register(CheckboxIngredient)
