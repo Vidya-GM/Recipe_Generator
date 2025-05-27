30b3fc19e9da00eae8382b7621163f3fbbc25cb7
Vidya <vidyag10@gmail.com>
2025-05-27 15:27:36 +0200

    Update admin.py of recipe for added models

--- culinary_cloud_ai/applications/recipes/admin.py
+++ culinary_cloud_ai/applications/recipes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from applications.recipes.models import Recipe, CookingTime, Cuisine, CheckboxIngredient
-from applications.recipes.models import Recipe, CookingTime, Cuisine, CheckboxIngredient
+from applications.recipes.models import Recipe, CookingTime, Cuisine, CheckboxIngredient, Like, Comment
+
 
 # Register your models here.
 
@@ -8,3 +8,5 @@ admin.site.register(Recipe)
 admin.site.register(CookingTime)
 admin.site.register(Cuisine)
 admin.site.register(CheckboxIngredient)
+admin.site.register(Like)
+admin.site.register(Comment)
